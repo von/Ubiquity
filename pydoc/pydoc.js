54d06e2e5bc5520dfbe31ca553c5830b0f061fcd
Von Welch <von@vwelch.com>
2010-12-05 22:24:38 -0600

    Updated to new Parser 2 API.

--- pydoc/pydoc.js
+++ pydoc/pydoc.js
@@ -1,14 +1,32 @@
-
-CmdUtils.CreateCommand({
-    name: "pydoc",
-    takes: {"Python term to search for": noun_arb_text},
-    author: "Von Welch",
-    email: "von@vwelch.com",
-    homepage: "http://www.vwelch.com/ubiquity/pydoc.html",
-    preview: "Uses http://docs.python.org/search.html to lookup python terms.",
-    execute: function( term ) {
-	var urlTemplate = "http://docs.python.org/search.html?q={QUERY}"
-	var urlString = urlTemplate.replace("{QUERY}", term.text);
-	Utils.openUrlInBrowser(urlString);
-    }
-})
+if (CmdUtils.parserVersion == 2) {
+   // Parser 2 (new) API
+   CmdUtils.CreateCommand({
+	name: ['pydoc'],
+	arguments: [ {role: 'object', nountype: noun_arb_text,
+		      label: "Python term to search for" } ],
+    	author: "Von Welch",
+    	email: "von@vwelch.com",
+    	homepage: "http://www.vwelch.com/ubiquity/pydoc.html",
+    	preview: "Uses http://docs.python.org/search.html to lookup python terms.",
+    	execute: function( args ) {
+	    var urlTemplate = "http://docs.python.org/search.html?q={QUERY}"
+	    var urlString = urlTemplate.replace("{QUERY}", args.object.text);
+	    Utils.openUrlInBrowser(urlString);
+    	}
+   })
+ } else {
+   // Parser 1 (old) API
+   CmdUtils.CreateCommand({
+	name: "pydoc",
+    	takes: {"Python term to search for": noun_arb_text},
+    	author: "Von Welch",
+    	email: "von@vwelch.com",
+    	homepage: "http://www.vwelch.com/ubiquity/pydoc.html",
+    	preview: "Uses http://docs.python.org/search.html to lookup python terms.",
+    	execute: function( term ) {
+	    var urlTemplate = "http://docs.python.org/search.html?q={QUERY}"
+	    var urlString = urlTemplate.replace("{QUERY}", term.text);
+	    Utils.openUrlInBrowser(urlString);
+    	}
+   })
+ }
